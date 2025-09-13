3b2260e3f00123d6531813bc6217ddb2dfcb6e84
Ethan Wong <ethaneswanebban@gmail.com>
2025-09-13 14:17:50 +1000

    deleted wrong one

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type UserType = {
+  id: string;
+  email: string;
+  displayName: string;
+}
+
+type AuthContextType = {
+  user: any | null;
+  token: string | null;
+  login: (token: string, user: UserType) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<UserType | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (newToken: string, newUser: any) => {
+    setToken(newToken);
+    setUser(newUser);
+    localStorage.setItem("token", newToken);
+    localStorage.setItem("user", JSON.stringify(newUser));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within AuthProvider");
+  return context;
+}
