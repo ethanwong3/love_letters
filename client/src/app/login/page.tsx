40f2495ad652422f006b2738699d734fa34deef7
Ethan Wong <ethaneswanebban@gmail.com>
2025-09-16 21:34:57 +1000

    typo

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -50,7 +50,7 @@ export default function LoginPage() {
     e.preventDefault();
     if (!validateFields()) return;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/auth/login`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email: email.trim(), password: password.trim() }),
